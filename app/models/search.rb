c1e05dfe9049ac083f563a7a3b5ed67cd3817570
Mirza Selim Reza <selimrezael@gmail.com>
2016-11-09 01:52:58 +0600

    advanced search functional

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,14 +1,20 @@
 class Search < ApplicationRecord
-  def search_mehmen
+  def mehmen
+    @mehmen ||= find_mehmen
+  end
+
+private
+  def find_mehmen
   	mehmen = Mehman.all
 
-  	mehmen = mehmen.where(['status LIKE ?', "%#{status}%"]) if status.present?
-  	mehmen = mehmen.where(['level LIKE ?', "%#{level}%"]) if level.present?
+  	mehmen = mehmen.where('status = ?', status) if status.present?
+  	mehmen = mehmen.where('level = ?', level) if level.present?
   	mehmen = mehmen.where(['code LIKE ? OR full_name LIKE ? OR passport_no LIKE ?' , "%#{keywords}%", "%#{keywords}%", "%#{keywords}%"]) if keywords.present?
-  	mehmen = mehmen.where(['arrival_date > ?', "%#{arrival_date_from}%"]) if arrival_date_from.present?
-  	mehmen = mehmen.where(['arrival_date < ?', "%#{arrival_date_to}%"]) if arrival_date_to.present?
-  	mehmen = mehmen.where(['visa_expiry_date > ?', "%#{visa_expiry_date_from}%"]) if visa_expiry_date_from.present?
-  	mehmen = mehmen.where(['visa_expiry_date > ?', "%#{visa_expiry_date_to}%"]) if visa_expiry_date_to.present?
+    mehmen = mehmen.where('country LIKE ?', country) if country.present?
+  	mehmen = mehmen.where('arrival_date >= ?', arrival_date_from) if arrival_date_from.present?
+  	mehmen = mehmen.where('arrival_date <= ?', arrival_date_to) if arrival_date_to.present?
+  	mehmen = mehmen.where('visa_expiry_date >= ?', visa_expiry_date_from) if visa_expiry_date_from.present?
+  	mehmen = mehmen.where('visa_expiry_date <= ?', visa_expiry_date_to) if visa_expiry_date_to.present?
 
   	return mehmen
   end
